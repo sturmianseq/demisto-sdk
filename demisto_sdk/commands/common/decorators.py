bb6070c7da7d77f7e83c416fcad74d7aa9075d4b
avidan-H <ahessing@paloaltonetworks.com>
2021-07-15 16:34:17 +0300

    Add debug decorator

---
+++ demisto_sdk/commands/common/decorators.py
@@ -0,0 +1,16 @@
+import functools
+from demisto_sdk.commands.common.logger import logger
+
+
+def debug(func):
+    """Print the function signature and return value"""
+    @functools.wraps(func)
+    def wrapper_debug(*args, **kwargs):
+        args_repr = [repr(a) for a in args]                      # 1
+        kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]  # 2
+        signature = ", ".join(args_repr + kwargs_repr)           # 3
+        logger.debug(f"Calling {func.__name__}({signature})")
+        value = func(*args, **kwargs)
+        logger.debug(f"{func.__name__!r} returned {value!r}")           # 4
+        return value
+    return wrapper_debug
